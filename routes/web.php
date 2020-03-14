9526ebd3a3970ef9334eeb9b4d1d3eb2d7c82bac
LoaiAkram <lolyakram11@gmail.com>
2020-03-14 22:45:31 +0200

    added pages controller

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index')->name('index');
+Route::get('/about', 'PagesController@about')->name('pages.about');
+Route::get('/contact', 'PagesController@contact')->name('pages.contact');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
